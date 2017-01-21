b16acd6fc86a824ad2f3d0f1c80a9bcb9bc2daea
bweben <nathanael.weber@bbcag.ch>
2017-01-21 11:57:06 +0100

    init, first runnable

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by natha on 21.01.2017.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My first App with Angular2 and Electron</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by natha on 21.01.2017.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
